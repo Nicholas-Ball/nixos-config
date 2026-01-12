1653d51ca5a80d6e98677eeaf03998da5186ceed
Nicholas-Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2026-01-12 10:39:57 -0500

    Fixed the screen issue

--- modules/home/hyprland/monitors.nix
+++ modules/home/hyprland/monitors.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 {
   wayland.windowManager.hyprland = {
-    settings.monitor = [ ",preferred,auto,auto" ];
+    settings.monitor = [
+      ",1920x1080,auto,1"
+      "DP-2,preferred,3840x0,1"
+      ",preferred,auto,1,mirror,eDP-1"
+    ];
 
     extraConfig = ''
       # hyprlang noerror true
